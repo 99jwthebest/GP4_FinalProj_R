daef1e35aadbea73c441418ae8d20bcf6199a760
99jwthebest <johern12@student.uiwtx.edu>
2024-05-05 11:37:25 -0500

    yeayhhg

--- Source/Gp4_Race_Final/Gp4_Race_FinalPawn.cpp
+++ Source/Gp4_Race_Final/Gp4_Race_FinalPawn.cpp
@@ -217,8 +217,11 @@ void AGp4_Race_FinalPawn::UpdateZoneLevel()
 	if (ZoneEnabled)
 	{
 		ChaosVehicleMovement->SetDownforceCoefficient(200.0f);
-		for(int i = 0; i < 3; i++)
-		ChaosVehicleMovement->SetTractionControlEnabled(i, true);
+		/*for (int i = 0; i > 1 && i < 3; i++)
+			ChaosVehicleMovement->SetWheelSlipGraphMultiplier(i, 6.0f);*/
+		/*for (int i = 0; i < 3 ; i++)
+			ChaosVehicleMovement->SetWheelSlipGraphMultiplier(i, 6.0f);*/
+
 		if (SteeringValue > 0.0f)
 		{
 			GetMesh()->AddForce(FVector::RightVector * ZoneSteering);
